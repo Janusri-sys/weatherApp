9024c81804de439e055271e92f504e7776402b9e
janusri-sys <januthota1234@gmail.com>
2023-05-22 18:59:26 +0530

    second commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function getWeatherData(cityValue){
         throw new Error("Network response is not ok")
     }
     const data=await response.json()
-    //console.log(data);
+    console.log(data);
     const temperature=Math.round(data.main.temp);
     const description=data.weather[0].description;
     const icon=data.weather[0].icon;
